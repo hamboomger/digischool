0abf5f6ee400152875084953178b9151d37b96f1
Denys Dorokhov <hamboomger@gmail.com>
2018-02-12 13:09:35 +0100

    StudentRegistrationWizard completed

--- src/main/java/com/digischool/registration/StudentRegistrationWizard.kt
+++ src/main/java/com/digischool/registration/StudentRegistrationWizard.kt
@@ -1,18 +1,42 @@
 package com.digischool.registration
 
 import com.digischool.registration.form.PersonalDataForm
+import com.digischool.registration.form.StudentIndexGenerationForm
 import com.digischool.registration.form.UserCredentialsForm
+import com.digischool.student.view.StudentPersonalDataView
+import com.digischool.user.Student
+import com.digischool.user.StudentModel
 import tornadofx.Wizard
 
 /**
  * @author ddorochov
  */
 class StudentRegistrationWizard : Wizard("Registration", "Students registration form") {
+    val registrationController: RegistrationController by di()
+
+    val personalDataModel: PersonalDataForm.PersonalDataModel by inject()
+    val userCredentialsModel: UserCredentialsForm.UserCredentialsModel by inject()
+
+    val studentModel: StudentModel by inject()
+
+    val student = Student()
+
     init {
+        personalDataModel.item = student
+        userCredentialsModel.item = student
+        studentModel.item = student
+
         root.prefWidth = 500.0
         root.prefHeight = 500.0
 
         add(PersonalDataForm::class)
         add(UserCredentialsForm::class)
+        add(StudentIndexGenerationForm::class)
     }
+
+    override fun onSave() {
+        registrationController.registerStudent(student)
+        replaceWith(StudentPersonalDataView::class)
+    }
+    
 }
