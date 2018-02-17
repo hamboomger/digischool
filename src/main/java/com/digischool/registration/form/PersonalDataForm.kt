068ea39edfd7e40d0c85b585636af957daa40e87
Denys Dorokhov <hamboomger@gmail.com>
2018-02-17 17:35:23 +0100

    Now email and login should be unique in registration

--- src/main/java/com/digischool/registration/form/PersonalDataForm.kt
+++ src/main/java/com/digischool/registration/form/PersonalDataForm.kt
@@ -1,26 +1,44 @@
 package com.digischool.registration.form
 
+import com.digischool.registration.RegistrationController
 import com.digischool.user.User
+import com.digischool.user.UsersManager
 import tornadofx.*
 
 /**
  * @author ddorochov
  */
 class PersonalDataForm : View("Personal data") {
+
+    private val usersManager: RegistrationController by inject()
+
     private val user: PersonalDataModel by inject()
 
     override val root = form {
         fieldset("Personal data") {
+
             field("Name") {
                 textfield(user.name).required()
             }
+
             field("Surname") {
                 textfield(user.surname).required()
             }
+
             field("Email") {
-                textfield(user.email).required()
+                textfield(user.email) {
+                    required()
+                    validator { checkEmailAlreadyInUse(it) }
+                }
             }
+
         }
+    }
+
+    private fun ValidationContext.checkEmailAlreadyInUse(it: String?): ValidationMessage? {
+        return if (it != null && usersManager.containsStudentWithEmail(it)) {
+            error("User with such email address already exists")
+        } else null
     }
 
     class PersonalDataModel: ItemViewModel<User>() {
--- src/main/java/com/digischool/registration/form/UserCredentialsForm.kt
+++ src/main/java/com/digischool/registration/form/UserCredentialsForm.kt
@@ -1,28 +1,49 @@
 package com.digischool.registration.form
 
+import com.digischool.registration.RegistrationController
 import com.digischool.user.User
+import com.digischool.user.UsersManager
+import com.digischool.util.requirements
 import tornadofx.*
 
 /**
  * @author ddorochov
  */
 class UserCredentialsForm : View("Credentials") {
+    private val usersManager: RegistrationController by inject()
     private val user: UserCredentialsModel by inject()
 
     override val root = form {
         fieldset("Credentials") {
             field("Login") {
-                textfield(user.login).required()
+                textfield(user.login) {
+                    required()
+                    validator { checkLoginAlreadyInUse(it) }
+                }
             }
+
             field("Password") {
-                passwordfield(user.password).required()
+                passwordfield(user.password) {
+                    required()
+                    requirements(minLength = 5)
+                }
             }
         }
+    }
+
+    private fun ValidationContext.checkLoginAlreadyInUse(it: String?): ValidationMessage? {
+        return if (it != null && usersManager.containsStudentWithLogin(it)) {
+            error("Login is already in use")
+        } else null
     }
 
     class UserCredentialsModel : ItemViewModel<User>() {
         val login = bind { item?.observable(User::login) }
         val password = bind { item?.observable(User::password) }
+    }
+
+    override fun onSave() {
+        isComplete = user.commit()
     }
 
 }
