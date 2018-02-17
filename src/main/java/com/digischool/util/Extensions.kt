06c22903b48054d9ed51b3e800ebba2d6cbc6cd7
Denys Dorokhov <hamboomger@gmail.com>
2018-02-17 17:37:01 +0100

    Extensions.kt: observableList() and observableSet() added

--- src/main/java/com/digischool/util/Extensions.kt
+++ src/main/java/com/digischool/util/Extensions.kt
@@ -1,7 +1,11 @@
 package com.digischool.util
 
 import javafx.beans.property.BooleanProperty
+import javafx.beans.property.SimpleListProperty
+import javafx.beans.property.SimpleSetProperty
 import javafx.beans.value.ObservableValue
+import javafx.collections.FXCollections
+import javafx.scene.control.PasswordField
 import tornadofx.*
 import java.util.*
 
@@ -18,4 +22,20 @@ fun Field.editableTextField(property: ObservableValue<String>, toggleEdit: Boole
 
 fun BooleanProperty.inverse() {
     value = !value
+}
+
+fun <T> observableList(list: List<T>?): SimpleListProperty<T> {
+    return SimpleListProperty(FXCollections.observableArrayList(list ?: emptyList()))
+}
+
+fun <T> observableSet(set: Set<T>?): SimpleSetProperty<T> {
+    return SimpleSetProperty(FXCollections.observableSet(set ?: emptySet()))
+}
+
+fun PasswordField.requirements(minLength: Int) {
+    validator {
+        if (it == null || it.length < minLength) {
+            error("You should have at least 5 symbols")
+        } else null
+    }
 }
